e88c06d705390eff9395fc772b8caf75a7cc953a
byeongcheollim <lim6112j@gmail.com>
2025-02-13 23:35:11 +0900

    natural language

---
+++ web/natural_language_toolkit.py
@@ -0,0 +1,37 @@
+from langchain.agents import AgentType, initialize_agent
+from langchain_community.agent_toolkits import NLAToolkit
+from langchain_community.utilities import Requests
+from langchain_openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
+llm = OpenAI(
+    temperature=0, max_tokens=700, model_name="gpt-3.5-turbo-instruct"
+)
+speak_toolkit = NLAToolkit.from_llm_and_url(llm, "https://api.speak.com/openapi.yaml")
+klarna_toolkit = NLAToolkit.from_llm_and_url(
+    llm, "https://www.klarna.com/us/shopping/public/openai/v0/api-docs/"
+)
+openai_format_instructions = """Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tool_names}]
+Action Input: what to instruct the AI Action representative.
+Observation: The Agent's response
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer. User can't see any of my observations, API responses, links, or tools.
+Final Answer: the final answer to the original input question with the right amount of detail
+
+When responding with your Final Answer, remember that the person you are responding to CANNOT see any of your Thought/Action/Action Input/Observations, so if there is any relevant information there you need to include it explicitly in your response.
+"""
+natural_language_tools = speak_toolkit.get_tools() + klarna_toolkit.get_tools()
+mrkl = initialize_agent(
+    natural_language_tools,
+    llm,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=True,
+    agent_kwargs={"format_instructions": openai_format_instructions},
+)
+mrkl.run(
+    "I have an end of year party for my Italian class and have to buy some Italian clothes for it"
+)
